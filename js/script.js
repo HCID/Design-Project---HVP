aefca9d4e7d54c96d28ad4ae7b5e1d8db19f0059
victor <victorvj26@gmail.com>
2013-03-20 00:56:40 +0100

    Exclusion
    When clicking a circle inside a group, the data is outside of this
    group is removed.
    Working always on selectedData variable!!!

--- js/script.js
+++ js/script.js
@@ -1,57 +1,175 @@
 var data = [],
     height = window.screen.availHeight,
 	  width = window.screen.availWidth,
-    grouping = 2;
+    grouping = 3;
     rScale = d3.scale.sqrt().range([0, 15]),
     fill = d3.scale.category20();
   
 // Array of forces 
 var forceData = [];
+var circleSelection = [];
+var force = [];
+var selectedData = [];
+
+var x = d3.scale.linear().domain([0, width]).range([0, width]),
+    y = d3.scale.linear().domain([0, height]).range([0, height]);
 
 var tick = function (e) {
   var k = 10 * e.alpha;
    d3.selectAll("g.circle_class")
     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })   		    
 };
+
   
 var split = function (dt, number) { 
 
   // Creating the number force arrays inside the forceData
+  forceData = [];
   for (var i=0;i<number;i++) { 
     forceData.push([]);
   }
 
+  if (number == 1) {
+    forceData[0] = dt.slice(0);
+  } else if (number == 2) {
+    
+    dt.forEach(function(o, i) {
+      if( (i % number)==0) {
+        forceData[0].push(o);
+      } else {
+        forceData[1].push(o);
+      }
+    });
+
+  } else if (number == 3) {
+    
+    dt.forEach(function(o, i) {
+      if( (i % number)==0) {
+        forceData[0].push(o);
+      } else if( (i % number)==1) {
+        forceData[1].push(o);
+      } else {
+        forceData[2].push(o);
+      }
+    });
+
+  }
   // Setting the data in each array
-  dt.forEach(function(o, i) {
-    if( (i % number)==0) {
-      forceData[0].push(o);
-    } else if( (i % number)==1) {
-      forceData[1].push(o);
-    } else {
-      forceData[2].push(o);
+  
+};
+
+// // zoom in / out
+// function zoom(d) {
+
+//   console.log("zooming");
+//   //vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+//   console.log(circleSelection[0].selectAll("g").selectAll("circle"));
+//   var nodes = circleSelection[0].selectAll("g").selectAll("circle");
+//   nodes.attr("transform", transform);
+
+//   // Update the links...
+//   // var link = vis.selectAll("path.link");
+//   // link.attr("d", translate);
+
+//   // Enter any new links at hte parent's previous position
+//   //link.attr("d", function(d) {
+//   //      var o = {x: d.x0, y: d.y0};
+//   //      return diagonal({source: o, target: o});
+//   //    });
+// } 
+
+// function transform(d) {
+//   console.log("transform");
+//   return "translate(" + x(d.y) + "," + y(d.x) + ")";
+// }
+
+// function translate(d) {
+//     console.log("translate");
+
+//   var sourceX = x(d.target.parent.y);
+//   var sourceY = y(d.target.parent.x);
+//   var targetX = x(d.target.y);
+//   var targetY = (sourceX + targetX)/2;
+//   var linkTargetY = y(d.target.x0);
+//   var result = "M"+sourceX+","+sourceY+" C"+targetX+","+sourceY+" "+targetY+","+y(d.target.x0)+" "+targetX+","+linkTargetY+"";
+//         //console.log(result);
+
+//   return result;
+// }
+
+// Restarts the data
+var restart = function() {
+
+  selectedData = [];
+  selectedData = data.slice(0);
+  twoFuntion();
+
+};
+
+var circleClicked = function(dt){
+
+  console.log("circleClicked #" + dt.id);
+  console.log(dt);
+  console.log(circleSelection);
+
+  //guessing in which array the element is placed 
+  var n = dt.id % grouping;
+  console.log("n " + n);
+  
+  // kind of works
+  console.log(circleSelection[n].selectAll("g").selectAll("#"+dt.id));
+  circleSelection[n].selectAll("g").selectAll("#"+dt.id).attr("transform", "translate(-10,-20)");
+  circleSelection[n].selectAll("g").selectAll("#"+dt.id).select("circle").attr("transform", "scale(4)");
+
+  selectedData = [];
+  selectedData = forceData[n].slice(0);
+  console.log("selectedData " + selectedData.length);
+  // d3.select("body").remove();
+
+  var t = d3.select("body").select("svg");
+  // $(t).fadeOut(400);
+  
+  for (var i=0;i<grouping;i++) { 
+    t.selectAll("#our_id"+i).transition().duration(2000).style("opacity", 0);
+  }
+
+  // setTimeout(function(){
+    for (var i=0;i<grouping;i++) { 
+      // circleSelection[i].remove();
+      t.selectAll("#our_id"+i).remove();
     }
-  });
+  // }, 500);
+  
+
+  // t.transition().duration(500).style("opacity", 1);
+
+  grouping = 1;
+  twoFuntion();
 
 };
 
 var twoFuntion = function () {
-  grouping = 3;
-  var force = [];
+  console.log("twoFuntion");
 
+  force = [];
   for (var i=0;i<grouping;i++) { 
-  
+    
+    force[i] = [];
     force[i] = d3
     .layout
     .force()
-    .gravity(0.3)
+    .gravity(0.15)
     .distance(500)
     .charge(function (d) { return d.forceR ? d.forceR*-4 : -35; } )
     .on("tick", tick);
 
   }
   
+  console.log("selectedData twoFuntion" + selectedData.length);
   //Time for spliting data or filtering
-  split(data, 3);
+  split(selectedData, grouping);
+
+  console.log("forceData " + selectedData.length);
 
   // Configuring forces
   for (var i=0;i<grouping;i++) {   
@@ -90,6 +208,8 @@ var twoFuntion = function () {
     circleSelection[j].enter().append("g")
       .attr("id", function(d, i){return d.id;})
       .attr("class", "circle_class")
+      .on("click", function(d){ circleClicked(d) } )
+      // .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1,8]).on("zoom",zoom))
       .append("circle")
       .attr("r", function (d) {
         return d.forceR;})
@@ -97,19 +217,20 @@ var twoFuntion = function () {
         return fill(i);})
       .style("stroke-width", 2)
       .style("stroke", function(d, i) { return d3.rgb(fill(i)).darker(2); })
-      .on("click", function(d) {alert("circle ", d);});
-//.call(force[j].drag); 
+      .call(force[j].drag); 
 
     circleSelection[j].exit().remove();
   }         
   
   // Custom positioning
   // Don't put into a loop
-  if (grouping == 2) {
+  if (grouping == 1) {
+    d3.select("#our_id0").attr("transform", "translate(-380, -100)");
+  } else if (grouping == 2) {
     d3.select("#our_id0").attr("transform", "translate(-380, -100)");
     d3.select("#our_id1").attr("transform", "translate(300, -100)"); 
-  }
-  if (grouping == 3) {
+  
+  } else if (grouping == 3) {
     d3.select("#our_id0").attr("transform", "translate(-380, -100)");
     d3.select("#our_id1").attr("transform", "translate(300, -100)");
     d3.select("#our_id2").attr("transform", "translate(100, -200)"); 
@@ -156,12 +277,13 @@ $(document).ready(function() {
 function yay () {
   d3.json('data/data.json', function (importedData) {
     data = importedData;
+    console.log("selectedData " + selectedData.length);
     minCollisionRadius = 5,
     maxDomainValue = d3.max(data, function(d) {
       return d.bookmarks;
     });
     rScale.domain([0, maxDomainValue]);
-    twoFuntion();
+    restart();
   });
 }
 
